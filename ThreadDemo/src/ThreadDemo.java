ce40ca239291cb2182c8bf5846fcbab0cb314223
TASNEEM <TASNEEM@DESKTOP-UOBA070>
2020-08-16 16:24:37 +0530

    First commit

---
+++ ThreadDemo/src/ThreadDemo.java
@@ -0,0 +1,54 @@
+
+class Thread1 extends Thread {
+	
+	public void run() {
+		show();
+	}
+	
+	public void show() {
+		for(int i = 0; i < 5; i++) {
+			System.out.println("Thread 1");
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
+
+class Thread2 extends Thread {
+	
+	public void run() {
+		show();
+	}
+	
+	public void show() {
+		for(int i = 0; i < 5; i++) {
+			System.out.println("Thread 2");
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
+
+public class ThreadDemo {
+
+	public static void main(String[] args) {
+		Thread1 obj1 = new Thread1();
+		Thread2 obj2 = new Thread2();
+		
+		obj1.start();
+		try {
+			Thread.sleep(10);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		obj2.start();
+	}
+
+}
+
